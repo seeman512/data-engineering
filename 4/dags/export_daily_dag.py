76fb5ab08d608d31eccf32485fdee0c85d9257a3
Semyon <seeman512@gmail.co>
2021-12-09 11:15:45 +0200

    Fixed daily dag

--- 4/dags/export_daily_dag.py
+++ 4/dags/export_daily_dag.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from datetime import datetime
+from datetime import datetime, date, timedelta
 import json
 
 from airflow import DAG, AirflowException
@@ -26,7 +26,11 @@ default_args = {
     'retries': 2,
 }
 
+yesterday = (date.today() - timedelta(days=1)).strftime("%Y-%m-%d")
+
 def export_from_api(date):
+    logging.info(f"Export from API by {date}")
+
     if not date:
         raise AirflowException("Required date param")
 
@@ -45,6 +49,8 @@ def export_from_api(date):
         json.dump(data, f)
 
 def export_from_db(date):
+    logging.info(f"Export order from db by {date}")
+
     if not date:
         raise AirflowException("Required date param")
 
@@ -53,8 +59,22 @@ def export_from_db(date):
     dir_path = os.path.join(app_config["directory"], date)
     os.makedirs(dir_path, exist_ok=True)
 
+    daily_query = """
+        SELECT *
+        FROM
+            orders o, clients c, products p,
+            aisles a, departments d
+        WHERE
+            o.order_date='{}'
+            and c.client_id=o.client_id
+            and p.product_id=o.product_id
+            and a.aisle_id=p.aisle_id
+            and d.department_id=p.department_id
+
+    """.format(date)
+
     with open(os.path.join(dir_path, "orders.csv"), "w") as f:
-        conn.cursor().copy_expert("COPY (select * from aisles) TO STDOUT WITH CSV HEADER", f)
+        conn.cursor().copy_expert(f"COPY ({daily_query}) TO STDOUT WITH CSV HEADER", f)
 
 dag = DAG(
     dag_id="export_daily_data",
@@ -62,8 +82,6 @@ dag = DAG(
     default_args=default_args,
     start_date=datetime(2021, 12, 6, 0, 0),
 )
-
-yesterday="2021-04-01"
 
 export_api=PythonOperator(
     task_id="export_from_api",

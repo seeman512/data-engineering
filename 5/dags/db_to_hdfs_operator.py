2f41d2243ad1dcf164da3c6d517b7f006e620a10
Semyon <seeman512@gmail.co>
2021-12-13 12:16:08 +0200

    Fixed initial task

--- 5/dags/db_to_hdfs_operator.py
+++ 5/dags/db_to_hdfs_operator.py
@@ -1,7 +1,9 @@
 from airflow.models.baseoperator import BaseOperator
 from airflow.utils.decorators import apply_defaults
 from airflow.hooks.postgres_hook import PostgresHook
-from airflow.hooks.hdfs_hook import HDFSHook
+from airflow.hooks.hdfs_hook import BaseHook
+
+from hdfs import InsecureClient
 import logging
 
 
@@ -14,20 +16,31 @@ class DbToHdfsOperator(BaseOperator):
             hdfs_conn_id="",
             db_table_name="",
             db_custom_query="",
+            hdfs_file_path="",
             *args, **kwargs):
         """
         :param db_conn_id: database connection id
         :param hdfs: hdfs connection id
         :param db_table_name: exported table
         :param db_custom_query: sql query if table is not provided
+        :param hdfs_file_path: hdfs file path
         """
 
         super().__init__(*args, **kwargs)
         self.db_conn = PostgresHook(db_conn_id).get_conn()
-        self.hdfs_conn = HDFSHook(hdfs_conn_id).get_conn()
         self.db_table_name = db_table_name
         self.db_custom_query = db_custom_query
+        self.hdfs_file_path = hdfs_file_path
+
+        hdfs_conn = BaseHook.get_connection(hdfs_conn_id)
+        self.hdfs_client = InsecureClient(f"http://{hdfs_conn.host}:{hdfs_conn.port}")
 
     def execute(self, context):
-        logging.info("EXEC CUSTOM OPERATOR")
-        logging.info(f"TABLE => {self.db_table_name}")
+        if self.db_table_name:
+            query = f"select * from {self.db_table_name}"
+        else:
+            query = self.db_custom_query
+
+        with self.hdfs_client.write(self.hdfs_file_path, overwrite=True) as f:
+            logging.info(f"Writing to {self.hdfs_file_path}")
+            self.db_conn.cursor().copy_expert(f"COPY ({query}) TO STDOUT WITH CSV HEADER", f)
--- 5/dags/export_initial_dag.py
+++ 5/dags/export_initial_dag.py
@@ -22,17 +22,6 @@ default_args = {
     'retries': 2,
 }
 
-def export_from_db():
-    conn = PostgresHook(app_config["pg_connection_id"]).get_conn()
-    for table in ["aisles", "clients", "departments", "orders", "products"]:
-        dir_path = os.path.join(app_config["directory"], "init")
-        os.makedirs(dir_path, exist_ok=True)
-
-        file_path = os.path.join(dir_path, f"{table}.csv")
-        with open(file_path, "w") as f:
-            conn.cursor().copy_expert(f"COPY (select * from {table}) TO STDOUT WITH CSV HEADER", f)
-            logging.info(f"Saved file {file_path}")
-
 dag = DAG(
     dag_id="export_initial_data",
     schedule_interval=None, # run dag only once manually
@@ -40,12 +29,17 @@ dag = DAG(
     default_args=default_args
 )
 
-export_db=DbToHdfsOperator(
-    task_id="export_from_db",
-    dag=dag,
-    db_conn_id=app_config["db_connection_id"],
-    hdfs_conn_id=app_config["hdfs_connection_id"],
-    db_table_name="ANY_TABLE"
-)
+tables = ["aisles", "clients", "departments", "location_areas", "orders",
+          "products", "store_types", "stores"]
 
-export_db
+task_kwargs = {
+    "dag": dag,
+    "db_conn_id": app_config["db_connection_id"],
+    "hdfs_conn_id": app_config["hdfs_connection_id"],
+}
+
+[ DbToHdfsOperator(task_id=f"export_from_db_{table}",
+                   db_table_name=table,
+                   hdfs_file_path=os.path.join(app_config['directory'], 'init', f"{table}.csv"),
+                   **task_kwargs)
+      for table in tables ]

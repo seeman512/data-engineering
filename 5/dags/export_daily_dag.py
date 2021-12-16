29fea1176c52363ac409236d7373d0cc6fec4a11
Semyon <seeman512@gmail.co>
2021-12-16 10:57:58 +0200

    Removed initial dag

--- 5/dags/export_daily_dag.py
+++ 5/dags/export_daily_dag.py
@@ -5,6 +5,7 @@ import json
 
 from airflow import DAG, AirflowException
 from airflow.models import Variable
+from airflow.utils.helpers import chain
 
 from config import Config
 from stock_api_to_hdfs_operator import StockApiToHdfsOperator
@@ -40,45 +41,35 @@ dag = DAG(
     start_date=datetime(2021, 12, 6, 0, 0),
 )
 
-hdfs_dir = os.path.join(app_config['directory'], yesterday)
+tables = ["aisles", "clients", "departments", "location_areas", "orders",
+          "products", "store_types", "stores"]
 
-export_api = StockApiToHdfsOperator(
-    task_id="export_from_api",
-    dag=dag,
-    hdfs_conn_id=app_config["hdfs_connection_id"],
-    http_conn_id=app_config["stock_api_connection_id"],
-    endpoint=app_config["endpoint"],
-    auth_endpoint=app_config["auth_endpoint"],
-    date=yesterday,
-    hdfs_file_path=os.path.join(hdfs_dir, "stock_api.csv"),
-    username=username,
-    password=password)
-
-daily_query = """
-    SELECT *
-    FROM
-        orders o, clients c, products p,
-        aisles a, departments d, stores s,
-        store_types st,
-        location_areas cla, location_areas sla
-    WHERE
-        o.order_date='{}'
-        and c.id=o.client_id
-        and p.product_id=o.product_id
-        and a.aisle_id=p.aisle_id
-        and d.department_id=p.department_id
-        and s.store_id=o.store_id
-        and st.store_type_id=s.store_type_id
-        and cla.area_id=s.location_area_id
-        and sla.area_id=s.location_area_id
-""".format(yesterday)
-
-export_db = DbToHdfsOperator(
-    task_id="export_from_db",
-    dag=dag,
-    db_conn_id=app_config["db_connection_id"],
-    hdfs_conn_id=app_config["hdfs_connection_id"],
-    hdfs_file_path=os.path.join(hdfs_dir, "db.csv"),
-    db_custom_query=daily_query)
-
-[export_api, export_db]
+[
+     StockApiToHdfsOperator(
+        task_id="export_from_api",
+        dag=dag,
+        hdfs_conn_id=app_config["hdfs_connection_id"],
+        http_conn_id=app_config["stock_api_connection_id"],
+        endpoint=app_config["endpoint"],
+        auth_endpoint=app_config["auth_endpoint"],
+        date=yesterday,
+        hdfs_file_path=os.path.join(app_config['directory'],
+                                    "stock_api",
+                                    yesterday,
+                                    "products.json"),
+        username=username,
+        password=password),
+    chain([
+        DbToHdfsOperator(
+            task_id=f"export_from_db_{table}",
+            dag=dag,
+            db_conn_id=app_config["db_connection_id"],
+            hdfs_conn_id=app_config["hdfs_connection_id"],
+            db_table_name=table,
+            hdfs_file_path=os.path.join(app_config['directory'],
+                                        "orders_db",
+                                        yesterday,
+                                        f"{table}.csv"))
+        for table in tables
+    ])
+]

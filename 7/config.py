1585f0b7391db2c8f267680b7906cfdb76c11ecf
Semyon <seeman512@gmail.co>
2022-01-04 12:55:21 +0200

    Fixed config struct

--- 7/config.py
+++ 7/config.py
@@ -2,12 +2,18 @@ import yaml
 
 
 class Config:
-    def __init__(self, path):
+    def __init__(self, path, app_name):
         with open(path, 'r') as f:
-            self.cfg = yaml.load(f, Loader=yaml.Loader)
+            self.cfg = yaml.load(f, Loader=yaml.Loader).get(app_name)
 
-    def app_config(self, app_name):
-        return self.cfg.get(app_name)
+    def stock_api(self):
+        return self.cfg.get("stock_api")
 
-    def logger_config(self):
-        return self.cfg.get("logger")
+    def fs(self):
+        return self.cfg.get("fs")
+
+    def connections(self):
+        return self.cfg.get("connections")
+
+    def db(self):
+        return self.cfg.get("db")
--- 7/dags/stock_api_dag.py
+++ 7/dags/stock_api_dag.py
@@ -20,12 +20,14 @@ from config import Config
 
 
 config_path = os.path.join(parent_dir, "config.yaml")
-app_name = "app"
-cfg = Config(config_path)
-app_config = cfg.app_config(app_name)
+app_name = "task7"
+cfg = Config(config_path, app_name)
+stock_api_config = cfg.stock_api()
+fs_config = cfg.fs()
+connections_config = cfg.connections()
 
-username = Variable.get("app_username", app_config["username"])
-password = Variable.get("app_password", app_config["password"])
+username = Variable.get("app_username", stock_api_config["username"])
+password = Variable.get("app_password", stock_api_config["password"])
 
 if not username or not password:
     raise AirflowException("Require api username or password in airflow variables or config.yaml")
@@ -38,7 +40,7 @@ default_args = {
 
 yesterday = (date.today() - timedelta(days=1)).strftime("%Y-%m-%d")
 
-spark_conn = BaseHook.get_connection(app_config["spark_connection_id"])
+spark_conn = BaseHook.get_connection(connections_config["spark_connection_id"])
 spark = SparkSession.builder\
     .master(spark_conn.host)\
     .appName("stock_api_dag")\
@@ -61,20 +63,20 @@ end_task = DummyOperator(
     dag=dag,
 )
 
-bronze_file_path = os.path.join(app_config["bronze_directory"],
+bronze_file_path = os.path.join(fs_config["bronze_directory"],
                                 "stock_api",
                                 yesterday,
                                 "products.json")
-silver_file_path = os.path.join(app_config["silver_directory"],
+silver_file_path = os.path.join(fs_config["silver_directory"],
                                 "stock_api_products")
 
 to_bronze_task = StockApiToHdfsOperator(
     task_id="from_stock_api_to_bronze",
     dag=dag,
-    hdfs_conn_id=app_config["hdfs_connection_id"],
-    http_conn_id=app_config["stock_api_connection_id"],
-    endpoint=app_config["endpoint"],
-    auth_endpoint=app_config["auth_endpoint"],
+    hdfs_conn_id=connections_config["hdfs_connection_id"],
+    http_conn_id=connections_config["stock_api_connection_id"],
+    endpoint=stock_api_config["endpoint"],
+    auth_endpoint=stock_api_config["auth_endpoint"],
     date=yesterday,
     hdfs_file_path=bronze_file_path,
     username=username,
